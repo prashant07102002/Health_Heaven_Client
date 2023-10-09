67bc59436f66b393b0368f1ba0a2dabdf67e3189
Prashant Patil <pp150549@gmail.com>
2023-10-09 14:07:04 +0530

    added signIn signUp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Navbar from "./components/Navbar";
 import GymSearch from "./scenes/GymSearch";
 import Home from "./scenes/Home";
-
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./scenes/SignIn";
+import SignUp from "./scenes/SignUp";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* <GymSearch /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gymSearch" element={<GymSearch />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar';
 import imageUrl from '../Assets/home_gif.gif';
+import { axiosClient } from '../Utils/axiosClient';
 
 const Home = () => {
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  async function fetchData(){
+    const response=await axiosClient.get('/getdata/userdata');
+    console.log("the respones is" ,response);
+  }
   return (
     <>
     <Navbar />
